ed56057cd2f645be73d7d2e39511c74f266c8032
22DP1AToca <128512901+22DP1AToca@users.noreply.github.com>
2024-03-13 13:06:31 +0000

    Iesākām objektu izgūšanu no faila

--- File/File.java
+++ File/File.java
@@ -2,20 +2,36 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 class File{
-    public List<List<String>> ReadFile(String file) throws FileNotFoundException, IOException{
-            List<List<String>> records = new ArrayList<>();
+    
+    public ArrayList<String> ReadFile(String file) throws FileNotFoundException, IOException{
+            ArrayList<String> records = new ArrayList<>();
             try (BufferedReader br = new BufferedReader(new FileReader(file))) {
                 String line;
             while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                records.add(Arrays.asList(values));
+               records.add(line);
             }}
-
         return records;
+    }
+
+    public ArrayList<Chocolates> getAllChocolates(chocolateCsvRows){
+           // mes izlasijam jellys csv
+           List<String> jellyCsvRows = new ArrayList<>();
+
+           // Atgriez ArrayList ar Jellys objektiem / return List<Jellyes>
+           List<Jellys> jellysList = ProductService.getAllJellys(jellyCsvRows);
+   
+           String rowExample = "1, sugar, 2kg, 2 euro";
+   
+           String[] rowParts = rowExample.split(", ");
+   
+           Jellys someJelly = new Jellys();
+           someJelly.setId(rowParts[0]);
+           someJelly.setId(rowParts[1]);
+           someJelly.setId(rowParts[2]);
+           someJelly.setId(rowParts[3]);
     }
 }
