bbecdba0b11126f8ba367990b1ef451c14801e94
Laky :) <88726120+Laky1006@users.noreply.github.com>
2024-03-19 10:08:32 +0000

    Nepabeigts failu reading - iesakts

--- App.java
+++ App.java
@@ -6,36 +6,37 @@ import java.util.List;
 
 class App{
     public static void main(String[] args) throws FileNotFoundException, IOException{
-        ArrayList chocolateObjects = new ArrayList<Chocolates>();
+        ArrayList<Chocolates> chocolateObjects = new ArrayList<Chocolates>();
 
-        File objFile = new File();
-        ArrayList<String> smth = objFile.ReadFile("/workspaces/notspringyveikals/CSV/chocolates.csv");
+        File chocolatesFile = new File("/workspaces/notspringyveikals/CSV/chocolates.csv", "'smth'", chocolateObjects);
+        // ArrayList<String> smth = 
+        chocolatesFile.getAll();
 
         // mes izlasijam jellys csv
-        List<String> jellyCsvRows = new ArrayList<>();
+        // List<String> jellyCsvRows = new ArrayList<>();
 
-        // Atgriez ArrayList ar Jellys objektiem / return List<Jellyes>
-        List<Jellys> jellysList = ProductService.getAllJellys(jellyCsvRows);
+        // // Atgriez ArrayList ar Jellys objektiem / return List<Jellyes>
+        // List<Jellys> jellysList = ProductService.getAllJellys(jellyCsvRows);
 
-        String rowExample = "1, sugar, 2kg, 2 euro";
+        // String rowExample = "1, sugar, 2kg, 2 euro";
 
-        String[] rowParts = rowExample.split(", ");
+        // String[] rowParts = rowExample.split(", ");
 
-        Jellys someJelly = new Jellys();
-        someJelly.setId(rowParts[0]);
-        someJelly.setId(rowParts[1]);
-        someJelly.setId(rowParts[2]);
-        someJelly.setId(rowParts[3]);
+        // Jellys someJelly = new Jellys();
+        // someJelly.setId(rowParts[0]);
+        // someJelly.setId(rowParts[1]);
+        // someJelly.setId(rowParts[2]);
+        // someJelly.setId(rowParts[3]);
 
-        for (List<String> x : smth){
-            Chocolates chocolates = new Chocolates();
+        // for (List<String> x : smth){
+        //     Chocolates chocolates = new Chocolates();
             
-            chocolates.name = x.get(0);
-            chocolates.price = Double.parseDouble(x.get(1));
-            chocolates.amountInStorage = Integer.parseInt(x.get(2));
-            chocolates.description = x.get(3);
+        //     chocolates.name = x.get(0);
+        //     chocolates.price = Double.parseDouble(x.get(1));
+        //     chocolates.amountInStorage = Integer.parseInt(x.get(2));
+        //     chocolates.description = x.get(3);
 
-            chocolateObjects.add(chocolates);
-        }
+        //     chocolateObjects.add(chocolates);
+        // }
     }
 }
--- File/File.java
+++ File/File.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.Console;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -6,10 +7,20 @@ import java.util.ArrayList;
 
 
 class File{
+
+    String filePath;
+    String type = "Chocolate";
+    ArrayList list;
+    Chocolates lalala = new Chocolates();
+    public File(String path, String type, ArrayList list){
+        filePath = path;
+        this.list = list;
+        
+    }
     
-    public ArrayList<String> ReadFile(String file) throws FileNotFoundException, IOException{
+    private ArrayList<String> ReadFile() throws FileNotFoundException, IOException{
             ArrayList<String> records = new ArrayList<>();
-            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
                 String line;
             while ((line = br.readLine()) != null) {
                records.add(line);
@@ -17,21 +28,52 @@ class File{
         return records;
     }
 
-    public ArrayList<Chocolates> getAllChocolates(chocolateCsvRows){
-           // mes izlasijam jellys csv
-           List<String> jellyCsvRows = new ArrayList<>();
 
-           // Atgriez ArrayList ar Jellys objektiem / return List<Jellyes>
-           List<Jellys> jellysList = ProductService.getAllJellys(jellyCsvRows);
-   
-           String rowExample = "1, sugar, 2kg, 2 euro";
-   
-           String[] rowParts = rowExample.split(", ");
-   
-           Jellys someJelly = new Jellys();
-           someJelly.setId(rowParts[0]);
-           someJelly.setId(rowParts[1]);
-           someJelly.setId(rowParts[2]);
-           someJelly.setId(rowParts[3]);
+    public void getAll() throws FileNotFoundException, IOException{
+        ArrayList<String> CSVrows = new ArrayList<String>();
+
+        CSVrows = ReadFile();
+        System.out.print(CSVrows);
+
+
+        for (String CSVrow : CSVrows) {
+            String[] rowParts = CSVrow.split(", ");
+
+            if(type == "Chocolate"){
+                Chocolates chocolateObj = new Chocolates();
+                chocolateObj.name = rowParts[0];
+                chocolateObj.price = Double.parseDouble(rowParts[0]);
+                chocolateObj.amountInStorage = Integer.parseInt(rowParts[0]);
+                chocolateObj.description = rowParts[0];
+            }
+
+            list.add(chocolateObj); //krc te kkas nasegaja ka man likas ka var saiet idk es vel pacakaresu sito lkm vnk neaiztikt
+        }
+        
+
+        
+        
     }
+
+    // NEKADA GADIJUM NE_ATKOMNETE SITO HUINU SAVADK VISS SAPLIST
+
+    // public ArrayList<Chocolates> getAllChocolates(chocolateCsvRows){
+    //        // mes izlasijam jellys csv
+    //        List<String> jellyCsvRows = new ArrayList<>();
+
+    //        // Atgriez ArrayList ar Jellys objektiem / return List<Jellyes>
+    //        List<Jellys> jellysList = ProductService.getAllJellys(jellyCsvRows);
+   
+    //        String rowExample = "1, sugar, 2kg, 2 euro";
+   
+    //        String[] rowParts = rowExample.split(", ");
+   
+    //        Jellys someJelly = new Jellys();
+    //        someJelly.setId(rowParts[0]);
+    //        someJelly.setId(rowParts[1]);
+    //        someJelly.setId(rowParts[2]);
+    //        someJelly.setId(rowParts[3]);
+    // }
+
+    
 }
