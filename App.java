f4680d61db85a36d8653b554ccf9c333a3eb3d75
Laky :) <88726120+Laky1006@users.noreply.github.com>
2024-03-25 07:51:23 +0000

    Need to fix problems with GitHub Codespace

--- App.java
+++ App.java
@@ -1,16 +1,25 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 class App{
     public static void main(String[] args) throws FileNotFoundException, IOException{
-        ArrayList<Chocolates> chocolateObjects = new ArrayList<Chocolates>();
+        ArrayList<Object> chocolateObjects = new ArrayList<Object>();
 
-        File chocolatesFile = new File("/workspaces/notspringyveikals/CSV/chocolates.csv", "'smth'", chocolateObjects);
+        Path file = Paths.get("CSV/chocolates.csv");
+        String path = file.toAbsolutePath() + "";
+        // System.out.println(paths);
+        File chocolatesFile = new File(path, "'smth'", chocolateObjects);
+
         // ArrayList<String> smth = 
         chocolatesFile.getAll();
+
+        System.out.println("Veikalā pieejamie produkti:");
+        System.out.println("----------------------------------");
+        chocolatesFile.check();
 
         // mes izlasijam jellys csv
         // List<String> jellyCsvRows = new ArrayList<>();
--- File/File.java
+++ File/File.java
@@ -1,21 +1,21 @@
 import java.io.BufferedReader;
-import java.io.Console;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-
+import java.lang.reflect.Field;
 
 class File{
 
     String filePath;
     String type = "Chocolate";
-    ArrayList list;
+
+    ArrayList<Object> list; 
     Chocolates lalala = new Chocolates();
-    public File(String path, String type, ArrayList list){
+
+    public File(String path, String type, ArrayList<Object> list){
         filePath = path;
         this.list = list;
-        
     }
     
     private ArrayList<String> ReadFile() throws FileNotFoundException, IOException{
@@ -28,31 +28,62 @@ class File{
         return records;
     }
 
+    //Prints out object
+    public void check(){
+        for (Object s: list){
+           //System.out.println(s);
+
+           //sis viss if ir Chat gpt
+           if (s instanceof Chocolates) {
+            Chocolates chocolateObj = (Chocolates) s;
+            // Use reflection to access fields
+            Field[] fields = Chocolates.class.getFields(); //get fields dabū publiskos arī inherited laukus klases
+            for (Field field : fields) {
+                try {
+                    //field.setAccessible(true); //vinam vajadzje parmainit lauka private -> public bet nez kpc to nedairja product klasei
+                    System.out.println(field.getName() + ": " + field.get(chocolateObj)); 
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+            }
+            System.out.println();
+        }
+    }
 
     public void getAll() throws FileNotFoundException, IOException{
         ArrayList<String> CSVrows = new ArrayList<String>();
 
-        CSVrows = ReadFile();
-        System.out.print(CSVrows);
+        CSVrows = ReadFile(); //[row1, row2]
 
-
-        for (String CSVrow : CSVrows) {
-            String[] rowParts = CSVrow.split(", ");
-
+        for (String CSVrow : CSVrows){
+            String[] rowParts = CSVrow.split(",");
             if(type == "Chocolate"){
                 Chocolates chocolateObj = new Chocolates();
                 chocolateObj.name = rowParts[0];
-                chocolateObj.price = Double.parseDouble(rowParts[0]);
-                chocolateObj.amountInStorage = Integer.parseInt(rowParts[0]);
-                chocolateObj.description = rowParts[0];
+                chocolateObj.price = Double.parseDouble(rowParts[1]);
+                chocolateObj.amountInStorage = Integer.parseInt(rowParts[2]);
+                chocolateObj.description = rowParts[3];
+                list.add(chocolateObj);
             }
-
-            list.add(chocolateObj); //krc te kkas nasegaja ka man likas ka var saiet idk es vel pacakaresu sito lkm vnk neaiztikt
         }
         
 
-        
-        
+        //TAVS VECAIS KODS
+        // for (String CSVrow : CSVrows) {
+        //     //String[] rowParts = CSVrow.split(", ");
+        //     //System.out.println(rowParts.length);
+
+        //     // if(type == "Chocolate"){
+        //     //     Chocolates chocolateObj = new Chocolates();
+        //     //     chocolateObj.name = rowParts[0];
+        //     //     chocolateObj.price = Double.parseDouble(rowParts[0]);
+        //     //     chocolateObj.amountInStorage = Integer.parseInt(rowParts[0]);
+        //     //     chocolateObj.description = rowParts[0];
+        //     // }
+
+        //     //list.add(chocolateObj); //krc te kkas nasegaja ka man likas ka var saiet idk es vel pacakaresu sito lkm vnk neaiztikt
+        // }
     }
 
     // NEKADA GADIJUM NE_ATKOMNETE SITO HUINU SAVADK VISS SAPLIST
