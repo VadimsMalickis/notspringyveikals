7bd9414ac601f111a8587ad8caf505226f7efa4d
Laky :) <88726120+Laky1006@users.noreply.github.com>
2024-03-27 08:53:57 +0000

    Shopping Cart - Get Set (doesnt work)

--- App.java
+++ App.java
@@ -14,25 +14,79 @@ class App{
         // Path file = Paths.get("CSV/chocolates.csv");
         // String path = file.toAbsolutePath() + "";
 
-        try (BufferedWriter writter = Utils.getWriter(ProductFile.CHOCOLATES.getFileName())) {
-            writter.newLine();
-            writter.write("Kikas special šokolāde,1000000.00," + new Random().nextInt(99));
-            writter.flush();
+        // try (BufferedWriter writter = Utils.getWriter(ProductFile.CHOCOLATES.getFileName())) {
+        //     writter.newLine();
+        //     writter.write("Kikas special šokolāde,1000000.00," + new Random().nextInt(99));
+        //     writter.flush();
         
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
 
 
-        try (BufferedReader reader = Utils.getReader("CSV/chocolates.csv")) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
+        // try (BufferedReader reader = Utils.getReader("CSV/chocolates.csv")) {
+        //     String line;
+        //     while ((line = reader.readLine()) != null) {
+        //         System.out.println(line);
+        //     }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
+
+
+        // LAKYS SHOPPING CART TEST CODE
+        // ArrayList<Products> smthAll = new ArrayList<Products>();
+
+        // Products testThing = new Chocolates();
+        // testThing.name = "aaaa";
+        // testThing.price = 666.666;
+        // testThing.amountInStorage = 2;
+        // testThing.description = "fdghjkl";
+
+        // smthAll.add(testThing);
+
+        // ShoppingCart usersShoppingCart = new ShoppingCart(smthAll);
+        // System.out.print(usersShoppingCart.getTotalPrice());
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         // File chocolatesFile = new File(path, "Chocolates", chocolateObjects);
 
