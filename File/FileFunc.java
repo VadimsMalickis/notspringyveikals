8cb11dfa262e680e2853bd70b20ea8775c2a7a60
22DP1AToca <128512901+22DP1AToca@users.noreply.github.com>
2024-03-30 15:57:39 +0000

    Added write file function

--- File/FileFunc.java
+++ File/FileFunc.java
@@ -2,19 +2,24 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 class FileFunc{
     String filePath;
-    String type;
-    ArrayList<Products> list;
 
-    public FileFunc(String path, String type, ArrayList<Products> list){
-        this.type = type;
+    public ArrayList<Products> chocolateObj= new ArrayList<>();
+    public ArrayList<Products> jellyObj= new ArrayList<>();
+    public ArrayList<Products> lollyObj= new ArrayList<>();
+    public ArrayList<ArrayList<Products>> productsObj = new ArrayList<ArrayList<Products>>();
+
+    public FileFunc(String path){
         filePath = path;
-        this.list = list; 
+
+        productsObj.add(chocolateObj);
+        productsObj.add(jellyObj);
+        productsObj.add(lollyObj);
     }
     
     private ArrayList<String> ReadFile() throws FileNotFoundException, IOException{
@@ -27,16 +32,17 @@ class FileFunc{
         return records;
     }
 
-    public void WriteFile(ArrayList<Products> list){
-
-        try (BufferedWriter writter = Utils.getWriter(ProductFile.CHOCOLATES.getFileName())) {
-            System.out.println("aaaa");
-            writter.newLine();
-            writter.write("alises želejas zobi, 10.00, 70, nav īsti zobi");
-            writter.flush();
-            
+    public void WriteFile(){
+        try (BufferedWriter writter = new BufferedWriter(new FileWriter("CSV/products.csv"))) {
+            for(int i = 0; i < productsObj.size(); i++){
+                for(int i2 = 0; i2 < productsObj.get(i).size(); i2++){
+                    Products temp = productsObj.get(i).get(i2);
+                    writter.write(temp.name + "," + temp.price + ","+ temp.amountInStorage + ","+ temp.type + ","+ temp.description);
+                    writter.newLine();
+                }
+            }
+            writter.close();         
         } catch (Exception e) {
-            System.out.println("help");
             e.printStackTrace();
         }
     }
@@ -53,13 +59,6 @@ class FileFunc{
     //     }
     // }
 
-    //Prints out object
-    public void check(){
-        for (Products s: list){
-                s.Display();
-        }
-    }
-
     public void GetAll() throws FileNotFoundException, IOException{
         ArrayList<String> CSVrows = new ArrayList<String>();
 
@@ -68,22 +67,27 @@ class FileFunc{
         for (String CSVrow : CSVrows){
             Products object = new Products();
 
-            String[] rowParts = CSVrow.split(",");
-            if(type == "Chocolate"){
+            String[] rowParts = CSVrow.split(","); //[nosaukums, cena, daudz,kategorija, apraksts]
+            String type = rowParts[3];
+
+            if(type.equals("Chocolate")){
                 object = new Chocolates();
+                chocolateObj.add(object);
             }
-            if(type == "Jellys"){
+            if(type.equals("Jelly")){
                 object = new Jellys();
+                jellyObj.add(object);
             }
-            if(type == "Lollies"){
+            if(type.equals("Lolly")){
                 object = new Lollies();
+                lollyObj.add(object);
             }
 
             object.name = rowParts[0];
             object.price = Double.parseDouble(rowParts[1]);
             object.amountInStorage = Integer.parseInt(rowParts[2]);
-            object.description = rowParts[3];
-            list.add(object);
+            object.type = type;
+            object.description = rowParts[4];
         }
     }
 }
