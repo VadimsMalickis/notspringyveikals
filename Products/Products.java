09e87c28a554f628d4b712055de36e7c9aa75224
22DP1AToca <128512901+22DP1AToca@users.noreply.github.com>
2024-03-30 15:57:29 +0000

    Added product type

--- Products/Products.java
+++ Products/Products.java
@@ -2,16 +2,19 @@ class Products{
     String name;
     double price;
     int amountInStorage;
+    String type;
     String description;
 
     public String toString(){
-        return name + ", " + price+ ", " + amountInStorage+ ", " + description;
+        return name + ", " + price+ ", " + amountInStorage + ", " + type + ", " + description;
     }
 
+    //so isti envajag vairak?
     public void Display(){
         System.out.println("Nosaukums: " + name);
         System.out.println("Cena: " + price);
         System.out.println("Daudzums noliktavā: " + amountInStorage);
+        System.out.println("Kategorija: " + type);
         System.out.println("Apraksts: " +  description);
         System.out.println();
     }
