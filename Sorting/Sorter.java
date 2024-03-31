6933087aa089c3bdbe7016f146db17567b778cf2
22DP1AToca <128512901+22DP1AToca@users.noreply.github.com>
2024-03-31 17:20:43 +0000

    Made SortName and descending order for SortPrice SortName

--- Sorting/Sorter.java
+++ Sorting/Sorter.java
@@ -3,17 +3,27 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class Sorter {
-    public void SortName(ArrayList<Products> list){
-        // Collections.sort(list, Comparator.comparingString(Products::getName));
-
-        // for (Products temp: list) {
-        //     System.out.println(temp);
-        // }
+    //Sort based on price
+    public void SortPrice(ArrayList<Products> list, boolean reversed){
+        if(!reversed){
+            Collections.sort(list, Comparator.comparingDouble(Products::getPrice));
+        }else{
+            Collections.sort(list, Comparator.comparingDouble(Products::getPrice).reversed());
+        }
+        
+        for (Products temp: list) {
+            System.out.println(temp);
+        }
     }
 
-    public void SortPrice(ArrayList<Products> list){
-        Collections.sort(list, Comparator.comparingDouble(Products::getPrice));
-
+    //Sort based on name
+    public void SortName(ArrayList<Products> list, boolean reversed){
+        if(!reversed){
+            list.sort((o1, o2) -> o1.getName().compareTo(o2.getName())); //lambda   (parameters) -> expression
+        }else{
+            list.sort(Collections.reverseOrder((o1, o2) -> o1.getName().compareTo(o2.getName())));
+        }
+    
         for (Products temp: list) {
             System.out.println(temp);
         }
