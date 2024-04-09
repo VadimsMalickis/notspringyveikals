51c7405db022fc9d0c499a13a0a4d45d74a4edd5
22DP1AToca <128512901+22DP1AToca@users.noreply.github.com>
2024-04-09 08:49:18 +0000

    Renamed Products -> Product

---
+++ Products/Product.java
@@ -0,0 +1,29 @@
+class Product{
+    String name;
+    double price; // eur for 1 paciņa
+    int amountInStorage; // amount of paciņas in storage
+    String type;
+    String description;
+
+    public String toString(){
+        return name + ", " + price+ ", " + amountInStorage + ", " + type + ", " + description;
+    }
+
+    public String getName(){
+        return name;
+    }
+    
+    public double getPrice(){
+        return price;
+    }
+
+    // //so isti envajag vairak?
+    // public void Display(){
+    //     System.out.println("Nosaukums: " + name);
+    //     System.out.println("Cena: " + price);
+    //     System.out.println("Daudzums noliktavā: " + amountInStorage);
+    //     System.out.println("Kategorija: " + type);
+    //     System.out.println("Apraksts: " +  description);
+    //     System.out.println();
+    // }
+}
