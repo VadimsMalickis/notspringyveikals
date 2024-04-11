9a18dbeea42d95c113922b0917f6fbf289342a11
22DP1AToca <128512901+22DP1AToca@users.noreply.github.com>
2024-04-11 19:22:00 +0000

    Completed Filter

--- Filtering/Filter.java
+++ Filtering/Filter.java
@@ -2,25 +2,18 @@ import java.util.ArrayList;
 
 public class Filter {
     ArrayList<Product> filteredList = new ArrayList<>();
-    
-    // ArrayList<Product> chocolateObj = new ArrayList<>();
-    // ArrayList<Product> jellyObj = new ArrayList<>();
-    // ArrayList<Product> lollyObj = new ArrayList<>();
 
     public  ArrayList<Product> FilterList(ArrayList<Product> list, boolean chocolateList, boolean jellyList, boolean lollyList){
         for (int i = 0; i < list.size(); i++) {
-            Product s = list.get(i);
-            String type = s.type;
-            System.out.println(type);
+            String type =  list.get(i).type;
 
-            if(type == "Chocolate" && chocolateList){
-                filteredList.add(list.get(i));
-                System.out.println("cg");
-            }
-            if(type == "Jelly" && jellyList){
+            if(type.equals("Chocolate") && chocolateList){
                 filteredList.add(list.get(i));
             }
-             if(type == "Lolly" && lollyList){
+            if(type.equals("Jelly") && jellyList){
+                filteredList.add(list.get(i));
+            }
+             if(type.equals( "Lolly") && lollyList){
                 filteredList.add(list.get(i));
             }
         }
