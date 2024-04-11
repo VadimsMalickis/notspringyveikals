0498c3b5b83a0b4110cbc3e0332bbdb98f991f62
22DP1AToca <128512901+22DP1AToca@users.noreply.github.com>
2024-04-11 08:32:08 +0000

    Started fixing Filter

--- Filtering/Filter.java
+++ Filtering/Filter.java
@@ -3,27 +3,27 @@ import java.util.ArrayList;
 public class Filter {
     ArrayList<Product> filteredList = new ArrayList<>();
     
-    public  ArrayList<Product> FilterList(ArrayList<Product> list,boolean chocolateList, boolean jellyList, boolean lollyList){
-        //RE-DO FILTER
-        
-        // if(chocolateList){
-        //     for (Product s : Products.getChocolates()) {
-        //         filteredList.add(s);
-        //     }
-        // }
-        // if(jellyList){
-        //     for (Product s : Products.getJelly()) {
-        //         filteredList.add(s);
-        //     }
-        // }
-        // if(lollyList){
-        //     for (Product s : Products.getLollies()) {
-        //         filteredList.add(s);
-        //     }
-        // }
-        System.out.println("Chocolates: " + chocolateList);
-        System.out.println("Jellys: " + jellyList);
-        System.out.println("Lollys: " + lollyList);
+    // ArrayList<Product> chocolateObj = new ArrayList<>();
+    // ArrayList<Product> jellyObj = new ArrayList<>();
+    // ArrayList<Product> lollyObj = new ArrayList<>();
+
+    public  ArrayList<Product> FilterList(ArrayList<Product> list, boolean chocolateList, boolean jellyList, boolean lollyList){
+        for (int i = 0; i < list.size(); i++) {
+            Product s = list.get(i);
+            String type = s.type;
+            System.out.println(type);
+
+            if(type == "Chocolate" && chocolateList){
+                filteredList.add(list.get(i));
+                System.out.println("cg");
+            }
+            if(type == "Jelly" && jellyList){
+                filteredList.add(list.get(i));
+            }
+             if(type == "Lolly" && lollyList){
+                filteredList.add(list.get(i));
+            }
+        }
 
         return filteredList;
     }
