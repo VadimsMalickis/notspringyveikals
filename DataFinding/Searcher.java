d67e8d9347dd87e6c8852717abfa25c36261ed44
22DP1AToca <128512901+22DP1AToca@users.noreply.github.com>
2024-04-25 08:29:50 +0000

    Search method complete

--- DataFinding/Searcher.java
+++ DataFinding/Searcher.java
@@ -1,18 +1,23 @@
 import java.util.ArrayList;
 
 public class Searcher {
-    ArrayList<Product> listTest;
-    public void Search(ArrayList<Product> list, String keyword){
-        
-    }
+    ArrayList<Product> seachedList; //????
 
-    public void Search(){
-        listTest.add(new Product(1,"kika", 2, 2, "Chocolate", "..."));
-        listTest.add(new Product(2,"kaija", 3.4, 9, "Chocolate", "..."));
-        listTest.add(new Product(3, "lapa", 1, 3, "Jelly", "..."));
+    public ArrayList<Product> Search(ArrayList<Product> list, String keyword){
+        seachedList = new ArrayList<>();
 
-        while (true) {
-            
+        for(Product temp : seachedList) {
+            if(temp.name.contains(keyword)){
+                seachedList.add(temp);
+            }
         }
+        return seachedList;
     }
+
+    // public String toString(){
+    //     for(Product temp : seachedList) {
+    //         String
+    //     }
+    //     return "";
+    // }
 }
